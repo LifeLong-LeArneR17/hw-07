b06e0373d567ae366f418778e88884f9cfb7c54d
LifeLong-LeArneR17 <sdelay.ne.sam@gmail.com>
2023-03-01 15:57:32 +0200

    fix
    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,26 +1,12 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-const galleryImages = document.querySelector('.gallery');
-const galleryEm = galleryItems => {
-    return galleryItems.map(({ preview, original, description }) => {
-        return `<div class=".gallery__item">
-            <img class="gallery__image"
-                src="${preview}"
-                alt="${description}"/>
-        </div>`;
-    })
-    .join("")
-}
-
-galleryImages.insertAdjacentHTML("beforeend", galleryEm(galleryItems))
 
 
-galleryImages.addEventListener('click', event => {
-
-})
+function (galleryItems)
 
 
-console.log(galleryItems);
 
 
-// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+
+
+
+
