5204c19893ed62d63bfe956329243f9c932415cd
LifeLong-LeArneR17 <sdelay.ne.sam@gmail.com>
2023-03-01 18:55:22 +0200

    fix
    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,11 +1,40 @@
 import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryImages = document.querySelector('.gallery');
+const galleryEm = galleryItems => {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}
+
+"/>
+ </a>
+        </div>`;
+    })
+        .join("")
+}
+
+const addGalleryMarkup = galleryEm(galleryItems);
+galleryImages.innerHTML = addGalleryMarkup;
 
 
-function (galleryItems)
 
+galleryImages.addEventListener("click", (e) => {
+    e.preventDefault()
+    if (e.target.nodeName !== "IMG") {
+        return
+    }
+    const selectedImages = e.target.getAttribute("data-source")
+    const instance = basicLightbox.create(`
+    <img src="${selectedImages}" width="800" height="600">
+`)
 
+    instance.show()
 
-
+})
 
 
 
